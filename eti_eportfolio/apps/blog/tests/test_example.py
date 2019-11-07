4b92638c0376d0b8a35a952f7cd4eb72027b5912
Kaioru <keith@kaioru.co>
2019-11-07 20:36:49 +0800

    Update workflow to install chrome and chromedriver

--- eti_eportfolio/apps/blog/tests/test_example.py
+++ eti_eportfolio/apps/blog/tests/test_example.py
@@ -1,2 +1,16 @@
+from selenium import webdriver
+
+
 def test_example():
     assert 1 + 1 == 2
+
+
+def test_web_example():
+    options = webdriver.ChromeOptions()
+
+    options.headless = True
+
+    driver = webdriver.Chrome(options=options)
+    driver.get('http://google.com')
+
+    assert 'Google' in driver.title
