522ba0ab3fc38bc27172890012c56547ad6241b5
Kaioru <keith@kaioru.co>
2019-11-08 09:13:16 +0800

    Move specific fixtures to proper files

--- eti_eportfolio/tests/conftest.py
+++ eti_eportfolio/tests/conftest.py
@@ -1,8 +1,4 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 @pytest.fixture(scope='module')
@@ -17,35 +13,3 @@ def driver():
     yield chrome
 
     chrome.close()
-
-
-@pytest.fixture
-def admin_login_page(driver, live_server, django_user_model):
-    driver.get(live_server.url + '/admin')
-
-    (WebDriverWait(driver, 3)
-     .until(EC.presence_of_element_located((By.ID, "content"))))
-
-    django_user_model.objects.create_superuser(
-        username='admin', email='admin@admin.com', password='password')
-
-
-@pytest.fixture
-def admin_page(driver, admin_login_page):
-    driver.find_element_by_name('username').send_keys('admin')
-    driver.find_element_by_name('password').send_keys('password')
-    driver.find_element_by_name('password').send_keys(Keys.RETURN)
-
-    (WebDriverWait(driver, 3)
-     .until(EC.presence_of_element_located((By.ID, "user-tools"))))
-
-
-@pytest.fixture
-def admin_change_password_page(driver, live_server, admin_page):
-    current_url = driver.current_url
-
-    driver.get(live_server.url + '/admin/password_change/')
-
-    WebDriverWait(driver, 3).until(EC.url_changes(current_url))
-    (WebDriverWait(driver, 3)
-     .until(EC.presence_of_element_located((By.NAME, "old_password"))))
--- eti_eportfolio/tests/test_view_blog_detail.py
+++ eti_eportfolio/tests/test_view_blog_detail.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,7 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from eti_eportfolio.apps.blog.models import Category, Post
 
 
-def test_view_blog_detail(driver, live_server):
+@pytest.fixture
+def blog_detail_page(driver, live_server):
     post = Post(title='A cool title', body='Lorem ipsum')
     category = Category(name='personal')
 
@@ -15,6 +17,11 @@ def test_view_blog_detail(driver, live_server):
 
     driver.get(live_server.url + '/blogs/' + str(post.id))
 
+    (WebDriverWait(driver, 3)
+     .until(EC.presence_of_element_located((By.TAG_NAME, "h1"))))
+
+
+def test_view_blog_detail(driver, blog_detail_page):
     (WebDriverWait(driver, 3)
      .until(EC.presence_of_element_located((By.TAG_NAME, "h1"))))
 
@@ -22,19 +29,7 @@ def test_view_blog_detail(driver, live_server):
     assert elem.text == 'A cool title'
 
 
-def test_create_comment_valid(driver, live_server):
-    post = Post(title='A cool title', body='Lorem ipsum')
-    category = Category(name='personal')
-
-    post.save()
-    category.save()
-    post.categories.add(category)
-
-    driver.get(live_server.url + '/blogs/' + str(post.id))
-
-    (WebDriverWait(driver, 3)
-     .until(EC.presence_of_element_located((By.TAG_NAME, "h1"))))
-
+def test_create_comment_valid(driver, blog_detail_page):
     driver.find_element_by_name('author').send_keys('Commenter')
     driver.find_element_by_name('body').send_keys('Cool blog post!')
     driver.find_element_by_tag_name('button').click()
