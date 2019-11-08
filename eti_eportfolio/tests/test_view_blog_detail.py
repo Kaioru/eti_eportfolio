341893f4cc7e64c94c424394c24ece69d003182b
Kaioru <keith@kaioru.co>
2019-11-08 08:20:17 +0800

    Add basic comment valid test

--- eti_eportfolio/tests/test_view_blog_detail.py
+++ eti_eportfolio/tests/test_view_blog_detail.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from eti_eportfolio.apps.blog.models import Category, Post
@@ -19,3 +20,27 @@ def test_view_blog_detail(driver, live_server):
 
     elem = driver.find_element_by_tag_name('h1')
     assert elem.text == 'A cool title'
+
+
+def test_create_comment_valid(driver, live_server):
+    post = Post(title='A cool title', body='Lorem ipsum')
+    category = Category(name='personal')
+
+    post.save()
+    category.save()
+    post.categories.add(category)
+
+    driver.get(live_server.url + '/blogs/' + str(post.id))
+
+    (WebDriverWait(driver, 3)
+     .until(EC.presence_of_element_located((By.TAG_NAME, "h1"))))
+
+    driver.find_element_by_name('author').send_keys('Commenter')
+    driver.find_element_by_name('body').send_keys('Cool blog post!')
+    driver.find_element_by_tag_name('button').click()
+
+    (WebDriverWait(driver, 3)
+     .until(EC.presence_of_element_located((By.ID, "comment-body"))))
+
+    comment = driver.find_element_by_id('comment-body')
+    assert comment.text == 'Cool blog post!'
