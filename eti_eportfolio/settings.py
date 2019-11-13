67145e1138d85bb315bf52b3636f0aa172b9a053
Kaioru <keith@kaioru.co>
2019-11-13 15:06:40 +0800

    Move templates to parent directory

--- eti_eportfolio/settings.py
+++ eti_eportfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'eti_eportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["eti_eportfolio/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
