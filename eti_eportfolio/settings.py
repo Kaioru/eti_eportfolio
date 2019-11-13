28c6b71eb1be54fc61177f53b5eaeafcc132cf20
Kaioru <keith@kaioru.co>
2019-11-13 14:58:21 +0800

    Add project subapp, models and migrations

--- eti_eportfolio/settings.py
+++ eti_eportfolio/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'eti_eportfolio.apps.blog'
+    'eti_eportfolio.apps.blog',
+    'eti_eportfolio.apps.project'
 ]
 
 MIDDLEWARE = [
