fd24f9c5e81e1e6f6c2b6d865174db3230f93640
Kaioru <keith@kaioru.co>
2019-11-21 20:39:10 +0800

    Add string funcs to model

--- eti_eportfolio/apps/project/models.py
+++ eti_eportfolio/apps/project/models.py
@@ -6,3 +6,6 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.ImageField(upload_to='projects/')
+
+    def __str__(self):
+        return self.title
